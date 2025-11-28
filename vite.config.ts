e89534e1a10aed1f6adabb506f1c22f85c007ff7
Wajid <abdulwajid845433@gmail.com>
2025-11-29 03:39:22 +0530

    Fix: Allow all hosts in Vite config for CodeSandbox

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,6 @@ export default defineConfig({
 			}
 		})
 	],
-	server: { port: 5173, host: true },
+	server: { port: 5173, host: true, allowedHosts: true },
 	preview: { port: 5173, host: true }
 });
